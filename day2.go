c36245cc64597300a1a78272f2b2e7c5fe892ed6
grantholly <gdholly@gmail.com>
2017-12-08 14:58:05 -0800

    adding second part of day 2

--- day2.go
+++ day2.go
@@ -16,18 +16,29 @@ func main() {
 	}
 	defer f.Close()
 
-	var sum int
+	var part_one_sum, part_two_sum int
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		row := strings.Split(scanner.Text(), "\t")
 		sorted_row := make([]int, len(row))
 		for i, v := range row {
 			sorted_row[i], _ = strconv.Atoi(v)
+			for j := 0; j < len(row); j++ {
+				if v != row[j] {
+					val, _ := strconv.Atoi(v)
+					other, _ := strconv.Atoi(row[j])
+					if val % other == 0 {
+						fmt.Println(val, other)
+						part_two_sum += val / other
+					}
+				}
+			}
 		}
 		sort.Slice(sorted_row, func(i, j int) bool {
 			return sorted_row[i] < sorted_row[j]
 		})
-		sum += sorted_row[len(sorted_row)-1] - sorted_row[0]
+		part_one_sum += sorted_row[len(sorted_row)-1] - sorted_row[0]
 	}
-	fmt.Println(sum)
+	fmt.Println(part_one_sum)
+	fmt.Println(part_two_sum)
 }
